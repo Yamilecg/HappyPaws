6aa332e08bf4a4e59207ae40e5b8287c2d364af2
Yaotecatl Martinez <yaotecatl.martinez@oracle.com>
2025-05-13 11:22:05 -0600

    feature: create a FF to test program section

--- test/selenium/features/steps/generic_steps.py
+++ test/selenium/features/steps/generic_steps.py
@@ -9,25 +9,51 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 from time import sleep
+url = "http://localhost:80"
 
-url = "https://www.google.com.mx/"
-service = FirefoxService(GeckoDriverManager().install())
+@given("initialize set up and variables")
+def set_up_environment(context):
+    context.element = ''
 
-@given("the user opens {page}")
+@given("the user opens the browser and searches for {page}")
 def open_browser(context, page):
-    context.driver = webdriver.Firefox(service=service)
-    available_pages = {'main page':'/'}
+    context.driver = webdriver.Firefox()
+    available_pages = {'main page':'/', 'program':'/#program'}
     try:
         context.driver.get(url + available_pages[page])  
-        sleep(5)
+    except Exception as e:
+        print("Ocurrió un error:", e)
+
+@then("verify section with id {id} exist")
+@then("verify {tag} containing {text} exist")
+def verify_tag(context, tag='', text='', id=''):
+    xpath = ''
+    if 'h' in tag.lower() or 'a' in tag.lower():
+        xpath = f'//{tag}[normalize-space()="{text}"]'
+        print(xpath)
+        try:
+            element = context.driver.find_element(By.XPATH, xpath)
+            assert element.text == text
+            context.element = element
+        except Exception as e:
+            print("Ocurrió un error:", e)
+    elif id != '':
+        try:
+            context.driver.find_element(By.ID, id)
+        except Exception as e:
+            print("Ocurrió un error:", e)
+
+@then("click on last element")
+def click_on_element(context):
+    try:
+        context.element.click()
     except Exception as e:
         print("Ocurrió un error:", e)
 
 @then("wait until {wait_type}")
 def wait_times(context, wait_type):
-    waiters = {"page is open":5}
-    if wait_type == "page is open":
-        context.wait.until(EC.presence_of_element_located((By.NAME, "q")))
+    waiters = {"page is open":2, "element is visible":3}
+    sleep(waiters["page is open"])
 
     
 @then("close all windows")
