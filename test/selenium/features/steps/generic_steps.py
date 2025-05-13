37643a128fee8f6a0768ae34fd7e3251f5a6606c
Yaotecatl Martinez <yaotecatl.martinez@oracle.com>
2025-05-13 11:46:56 -0600

    fix: fixed format for feature file and steps files

--- test/selenium/features/steps/generic_steps.py
+++ test/selenium/features/steps/generic_steps.py
@@ -1,34 +1,31 @@
-from behave import given, when, then
+from time import sleep
+from behave import given, then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
 
-from time import sleep
-url = "http://localhost:80"
+URL = "http://localhost:80"
+
 
 @given("initialize set up and variables")
 def set_up_environment(context):
-    context.element = ''
+    context.element = ""
+
 
 @given("the user opens the browser and searches for {page}")
 def open_browser(context, page):
     context.driver = webdriver.Firefox()
-    available_pages = {'main page':'/', 'program':'/#program'}
+    available_pages = {"main page": "/", "program": "/#program"}
     try:
-        context.driver.get(url + available_pages[page])  
+        context.driver.get(URL + available_pages[page])
     except Exception as e:
         print("Ocurrió un error:", e)
 
+
 @then("verify section with id {id} exist")
 @then("verify {tag} containing {text} exist")
-def verify_tag(context, tag='', text='', id=''):
-    xpath = ''
-    if 'h' in tag.lower() or 'a' in tag.lower():
+def verify_tag(context, tag="", text="", id=""):
+    xpath = ""
+    if "h" in tag.lower() or "a" in tag.lower():
         xpath = f'//{tag}[normalize-space()="{text}"]'
         print(xpath)
         try:
@@ -37,11 +34,12 @@ def verify_tag(context, tag='', text='', id=''):
             context.element = element
         except Exception as e:
             print("Ocurrió un error:", e)
-    elif id != '':
+    elif id != "":
         try:
             context.driver.find_element(By.ID, id)
         except Exception as e:
             print("Ocurrió un error:", e)
+
 
 @then("click on last element")
 def click_on_element(context):
@@ -50,12 +48,13 @@ def click_on_element(context):
     except Exception as e:
         print("Ocurrió un error:", e)
 
+
 @then("wait until {wait_type}")
 def wait_times(context, wait_type):
-    waiters = {"page is open":2, "element is visible":3}
-    sleep(waiters["page is open"])
+    waiters = {"page is open": 2, "element is visible": 3}
+    sleep(waiters[wait_type])
 
-    
+
 @then("close all windows")
 def after_scenario(context):
     context.driver.quit()
