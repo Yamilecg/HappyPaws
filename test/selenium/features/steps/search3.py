ad961b2f119ce3e669b59903219182b506bacc9c
Yamile <111326511+Yamilecg@users.noreply.github.com>
2025-05-13 10:27:18 -0600

    Selenium yami (#23)
    * primeros cambios de selenium
    
    * pruebas s
    
    * search 3

--- test/selenium/features/steps/search3.py
+++ test/selenium/features/steps/search3.py
@@ -1,22 +1,34 @@
 from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-@given('El usuario está en la página de historial de adopción')
-def step_impl(context):
+@given('El usuario está en la página de perritos')
+def step_open_dog_page(context):
     context.driver = webdriver.Chrome()
-    context.driver.get('http://127.0.0.1:5500/views/userAdoptions.html')
-    context.driver.maximize_window()
-    time.sleep(1)
+    context.driver.get("http://localhost/index.html#portfolio")
 
-@when('El usuario hace clic en el botón "Agregar"')
-def step_impl(context):
-    agregar_link = context.driver.find_element(By.XPATH, "//a[contains(@href, 'uploadDog.html')]")
-    agregar_link.click()
-    time.sleep(1)
+    WebDriverWait(context.driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'portfolio'))
+    )
 
-@then('El navegador redirige a la página "uploadDog.html"')
-def step_impl(context):
-    assert "uploadDog.html" in context.driver.current_url
+@when('El usuario selecciona "Husky" en el filtro de raza')
+def step_select_husky_breed(context):
+    breed_dropdown = WebDriverWait(context.driver, 10).until(
+        EC.visibility_of_element_located((By.ID, 'filtro'))
+    )
+    select = Select(breed_dropdown)
+    select.select_by_visible_text("Husky")
+
+    time.sleep(5)  # Espera para visualizar el cambio, puedes ajustar o eliminar
+
+@then('El filtro de raza muestra "Husky"')
+def step_verify_husky_displayed(context):
+    selected_breed = context.driver.find_element(By.ID, 'filtro')
+    selected_breed_value = selected_breed.get_attribute('value')
+
+    assert selected_breed_value == "Husky"
+
     context.driver.quit()
