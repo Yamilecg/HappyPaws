6ea633ba3f760c7f6899a88804f0b36f169bbe1b
Yaotecatl Martinez <yaotecatl.martinez@oracle.com>
2025-05-13 11:57:40 -0600

    fix: all step files are now fixed

--- test/selenium/features/steps/search2.py
+++ test/selenium/features/steps/search2.py
@@ -4,11 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from behave import given, when, then
 
-@given('El usuario abre la página de descripción del perro')
+
+@given("El usuario abre la página de descripción del perro")
 def step_open_page(context):
     context.driver = webdriver.Chrome()
-    context.driver.get('http://127.0.0.1:5500/views/dogDescription.html')   
+    context.driver.get("http://127.0.0.1:5500/views/dogDescription.html")
     context.driver.maximize_window()
+
 
 @when('Selecciona la raza "{breed}"')
 def step_select_breed(context, breed):
@@ -17,9 +19,11 @@ def step_select_breed(context, breed):
     ).click()
 
     raza_element = WebDriverWait(context.driver, 15).until(
-        EC.presence_of_element_located((By.XPATH, f"//a[@data-value='{breed}']"))
+        EC.presence_of_element_located(
+            (By.XPATH, f"//a[@data-value='{breed}']"))
     )
     raza_element.click()
+
 
 @then('Se muestra información relacionada con "{breed}"')
 def step_show_breed_info(context, breed):
